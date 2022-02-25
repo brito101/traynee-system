090b271026582488f89b3bd3965386c8919cbe7d
brito101 <rodrigobrito101@hotmail.com>
2022-02-25 18:00:56 -0300

    Seeder user

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -13,6 +14,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::create([
+            'name'      => 'Administrator',
+            'email'     => 'admin@estagio.com',
+            'password'  => bcrypt('12345678'),
+        ]);
     }
 }
