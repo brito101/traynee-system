d30225b8178f92e2e09c3cfc930c9c987c2617e0
brito101 <rodrigobrito101@hotmail.com>
2022-02-25 17:39:05 -0300

    Initial settings and migration

---
+++ database/migrations/2022_02_25_171900_create_companies_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreignId('user_id');
+            $table->string('social_name');
+            $table->string('alias_name')->nullable();
+            $table->string('document_company')->nullable();
+            $table->string('document_company_secondary')->nullable();
+            /** address */
+            $table->string('zipcode')->nullable();
+            $table->string('street')->nullable();
+            $table->string('number')->nullable();
+            $table->string('complement')->nullable();
+            $table->string('neighborhood')->nullable();
+            $table->string('state')->nullable();
+            $table->string('city')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
